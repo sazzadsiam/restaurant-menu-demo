36ab43f09542a032f5b4b39186d1ac3a5e2702a1
Sazzad Siam <sazzadsiam113@gmail.com>
2024-09-09 23:47:09 +0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,139 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+import { useMediaQuery } from "@/hooks/use-media-query";
+
+// import { Drawer, DrawerContent } from "@/components/ui/drawer";
+import { Sheet, SheetContent } from "@/components/ui/sheet";
+
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import logo from "@/public/images/logo.png";
+import Menu from "@/components/Menu";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+const HomePage = () => {
+  const [menu, setMenu] = useState(false);
+  const [app, setApp] = useState(false);
+  // const [review, setReveiw] = useState(false);
+  // const [story, setStory] = useState(false);
+
+  const lgScreen = useMediaQuery("(min-width: 768px)");
+  const [height, setHeight] = useState(0);
+  const shape = useRef(null);
+
+  useEffect(() => {
+    const element = shape.current;
+
+    if (element) {
+      // Create a ResizeObserver to observe changes in the element's height
+      const observer = new ResizeObserver((entries) => {
+        // Get the height of the element
+        const newHeight = entries[0].contentRect.height;
+        setHeight(newHeight);
+      });
+
+      // Start observing the element
+      observer.observe(element);
+
+      // Clean up the observer on unmount
+      return () => observer.disconnect();
+    }
+  }, []);
+
+  return (
+    <>
+      <main className="min-h-[800px] h-screen relative">
+        <section className="flex gap-x-4 text-white absolute right-1/2 translate-x-1/2 tablet:translate-x-0  tablet:right-16 top-4 items-center">
+          <img src="./icons/fac.svg" alt="instagram icon" />
+          <img src="./icons/insta.svg" alt="facebook icon" />
+          <p>Contact us</p>
+        </section>
+        <section className="flex flex-col gap-y-16 items-center h-screen pt-[20vh] laptop:pt-[8vh]">
+          <Image
+            src={logo}
+            alt="logo"
+            priority
+            className="min-w-[360px] w-[32vw]"
+          />
+          <button
+            className="px-10 py-4 bg-primary text-white font-bold text-xl tablet:text-[1.5vw] rounded-sm"
+            onClick={() => setMenu(true)}
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+            Menu
+          </button>
+        </section>
+        <section
+          style={{ height: height }}
+          className="absolute bottom-0 left-0 w-full"
+        >
+          <div
+            className="w-full flex gap-6 tablet:gap-16 laptop:gap-40 flex-shrink-0 justify-center z-[1] absolute"
+            style={{ bottom: height / 2 }}
+          >
+            <button
+              className="w-[100px] tablet:w-[130px] aspect-square bg-white text-primary flex flex-col justify-center gap-y-2 items-center rounded-sm font-bold flex-shrink-0"
+              onClick={() => setApp(true)}
+            >
+              <img
+                src="./icons/mobile-icon.svg"
+                alt="mobile icon"
+                className="w-9"
+              />
+              <p>App</p>
+            </button>
+            <button className="w-[100px] tablet:w-[130px] aspect-square bg-white text-primary flex flex-col justify-center gap-y-2 items-center rounded-sm font-bold flex-shrink-0">
+              <img
+                src="./icons/review-icon.svg"
+                alt="review icon"
+                className="w-11"
+              />
+              <p>Reviews</p>
+            </button>
+            <button className="w-[100px] tablet:w-[130px] aspect-square bg-white text-primary flex flex-col justify-center gap-y-2 items-center rounded-sm font-bold flex-shrink-0">
+              <img
+                src="./icons/book-icon.svg"
+                alt="story icon"
+                className="w-9"
+              />
+              <p>Our story</p>
+            </button>
+          </div>
+          {lgScreen ? (
+            <img
+              ref={shape}
+              src="./icons/round-shape.svg"
+              alt="illustration"
+              className="w-full absolute bottom-0"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          ) : (
+            <img
+              ref={shape}
+              src="./icons/round-shape-mobile.svg"
+              alt="illustration"
+              className="w-full absolute bottom-0"
+            />
+          )}
+        </section>
+        <Sheet open={menu} onOpenChange={(open) => setMenu(open)}>
+          <SheetContent
+            side={"bottom"}
+            className="max-w-[100vw] w-full h-[100%] bg-white"
           >
-            Read our docs
-          </a>
-        </div>
+            <Menu />
+          </SheetContent>
+        </Sheet>
+
+        <Sheet open={app} onOpenChange={(open) => setApp(open)}>
+          <SheetContent
+            side={"bottom"}
+            className="max-w-[100vw] w-full bg-white h-[50vh]"
+          >
+            <p>Comming soon</p>
+          </SheetContent>
+        </Sheet>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
-}
+};
+export default HomePage;
---
+++ components/Menu.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { ScrollArea } from "./ui/scroll-area";
+
+import Image from "next/image";
+
+const Menu = () => {
+  return (
+    <div>
+      <nav className="flex gap-4 tablet:gap-12 w-full justify-center items-center pt-6 flex-wrap">
+        <button className="px-[18px] py-3 bg-primary text-white border-neutral-200 drop-shadow-sm font-medium rounded-sm">
+          All
+        </button>
+        <button className="px-[18px] py-3 bg-white text-black border border-neutral-200 drop-shadow-sm font-medium rounded-sm">
+          Starter
+        </button>
+        <button className="px-[18px] py-3 bg-white text-black border border-neutral-200 drop-shadow-sm font-medium rounded-sm">
+          Wok specialties
+        </button>
+        <button className="px-[18px] py-3 bg-white text-black border border-neutral-200 drop-shadow-sm font-medium rounded-sm">
+          Add on
+        </button>
+        <button className="px-[18px] py-3 bg-white text-black border border-neutral-200 drop-shadow-sm font-medium rounded-sm">
+          Drinks
+        </button>
+        <button className="px-[18px] py-3 bg-white text-black border border-neutral-200 drop-shadow-sm font-medium rounded-sm">
+          Special dishes
+        </button>
+      </nav>
+
+      <ScrollArea style={{ height: "80vh" }}>
+        <section className="grid grid-cols-1 tablet:grid-cols-2 gap-6 mt-10">
+          <div className="flex gap-x-4 bg-white border border-neutral-300 drop-shadow-sm p-6 rounded-md">
+            <Image
+              src="/images/food-one.png"
+              alt="food image"
+              width={280}
+              height={220}
+              className="w-[140px] h-[110px] tablet:w-[180px] tablet:h-[150px] laptop:w-[280px] laptop:h-[220px] object-cover rounded-sm"
+            />
+            <div className="flex flex-col gap-y-2">
+              <h2 className="text-xl font-semibold">
+                Chicken Skewers with Peanut Sauce
+              </h2>
+              <p className="text-sm">
+                Cauliflower, squash, onions, peppers, carrots and tofu, in a
+                flavorful shacha sauce
+              </p>
+              <p>
+                <span className="text-sm font-bold">With your choice of: </span>
+                <span className="text-sm">Chicken | Beef | Shrimp</span>
+              </p>
+              <div className="flex gap-x-2">
+                <span className="text-sm font-bold">Price:</span>
+                <div className="text-sm flex flex-col gap-y-1">
+                  <span>Chicken- 35 AED</span>
+                  <span>Beef- 37 AED</span>
+                  <span>Shrimp- 39 AED</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex gap-x-4 bg-white border border-neutral-300 drop-shadow-sm p-6 rounded-md">
+            <Image
+              src="/images/food-one.png"
+              alt="food image"
+              width={280}
+              height={220}
+              className="w-[140px] h-[110px] tablet:w-[180px] tablet:h-[150px] laptop:w-[280px] laptop:h-[220px] object-cover rounded-sm"
+            />
+            <div className="flex flex-col gap-y-2">
+              <h2 className="text-xl font-semibold">
+                Chicken Skewers with Peanut Sauce
+              </h2>
+              <p className="text-sm">
+                Cauliflower, squash, onions, peppers, carrots and tofu, in a
+                flavorful shacha sauce
+              </p>
+              <p>
+                <span className="text-sm font-bold">With your choice of: </span>
+                <span className="text-sm">Chicken | Beef | Shrimp</span>
+              </p>
+              <div className="flex gap-x-2">
+                <span className="text-sm font-bold">Price:</span>
+                <div className="text-sm flex flex-col gap-y-1">
+                  <span>Chicken- 35 AED</span>
+                  <span>Beef- 37 AED</span>
+                  <span>Shrimp- 39 AED</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </ScrollArea>
+    </div>
+  );
+};
+export default Menu;
